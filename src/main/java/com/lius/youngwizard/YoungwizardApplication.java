6c536b7e9a6948ebcf9ad6991e490ebe00a20e23
刘逸曦 <liuyixi2009@163.com>
2022-01-01 23:51:33 +0800

    feat: init

---
+++ src/main/java/com/lius/youngwizard/YoungwizardApplication.java
@@ -0,0 +1,13 @@
+package com.lius.youngwizard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoungwizardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoungwizardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lius/youngwizard/YoungwizardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lius.youngwizard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoungwizardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
